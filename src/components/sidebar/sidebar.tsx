62d35656597e8514e8324e1596f8bd7d77be9018
rakss17 <bohariambulo17@gmail.com>
2023-07-08 21:45:05 +0800

    added sidebar component

---
+++ src/components/sidebar/sidebar.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./sidebar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faColumns,
+  faClipboardList,
+  faCar,
+  faCalendarAlt,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+
+const sidebarData = [
+  { icon: faColumns, text: "Dashboard" },
+  { icon: faClipboardList, text: "Requests" },
+  { icon: faCar, text: "Vehicles" },
+  { icon: faCalendarAlt, text: "Schedules" },
+  { icon: faUser, text: "Drivers" },
+];
+
+export default function Sidebar() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <>
+      <div className={`containerSB ${isSidebarOpen ? "open" : ""}`}>
+        <div className="sidebar">
+          <button className="toggle-button" onClick={toggleSidebar}>
+            <FontAwesomeIcon icon={faBars} className="icon-bar" />
+          </button>
+          <div className="sidebar-buttons">
+            {sidebarData.map((item, index) => (
+              <button className="sidebar-button" key={index}>
+                <FontAwesomeIcon icon={item.icon} className="sidebar-icon" />
+                <span className="sidebar-text">{item.text}</span>
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/OfficeStaff/DashboardOS/dashboardOS.tsx
+++ src/pages/OfficeStaff/DashboardOS/dashboardOS.tsx
@@ -1,11 +1,13 @@
 import "./dashboardOS.css";
 import Header from "../../../components/header/header";
+import Sidebar from "../../../components/sidebar/sidebar";
 
 export default function DashboardOS() {
   return (
     <>
       <div className="containerOS">
         <Header />
+        <Sidebar />
       </div>
     </>
   );

d04d51c95e125049b169ec7d2fc93fec5de223e8
rakss17 <bohariambulo17@gmail.com>
2023-07-09 19:46:57 +0800

    today's trip and calendar for schedules dashboard

--- src/pages/OfficeStaff/DashboardOS/dashboardOS.tsx
+++ src/pages/OfficeStaff/DashboardOS/dashboardOS.tsx
@@ -1,13 +1,23 @@
 import "./dashboardOS.css";
 import Header from "../../../components/header/header";
 import Sidebar from "../../../components/sidebar/sidebar";
+import CalendarSchedule from "../../../components/calendar/calendar";
 
 export default function DashboardOS() {
   return (
     <>
+      <Header />
+      <Sidebar />
+
       <div className="containerOS">
-        <Header />
-        <Sidebar />
+        <h1>Dashboard</h1>
+        <div className="dashboard-container">
+          <div className="today-trip">
+            <p>Today's Trip</p>
+            <h2>10</h2>
+          </div>
+          <CalendarSchedule />
+        </div>
       </div>
     </>
   );

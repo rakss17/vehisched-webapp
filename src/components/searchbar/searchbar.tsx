8556201f2e6bbf71278b7e6a226cb2e8580ac006
rakss17 <bohariambulo17@gmail.com>
2023-07-12 12:35:12 +0800

    initial searchbar component

---
+++ src/components/searchbar/searchbar.tsx
@@ -0,0 +1,46 @@
+import "./searchbar.css";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import React, { useState } from "react";
+
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    onSearch(searchQuery);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <div className="inputsearch">
+          <div className="icon-container">
+            <FontAwesomeIcon icon={faSearch} className="input-icon" />
+          </div>
+
+          <input
+            className="search-bar"
+            type="text"
+            placeholder="Search"
+            value={searchQuery}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        {/* <button type="submit">Search</button> */}
+      </form>
+    </>
+  );
+};
+
+export default SearchBar;
--- src/pages/OfficeStaff/Requests/requests.tsx
+++ src/pages/OfficeStaff/Requests/requests.tsx
@@ -3,13 +3,23 @@ import Sidebar from "../../../components/sidebar/sidebar";
 import Container from "../../../components/container/container";
 import "./requests.css";
 import Label from "../../../components/label/label";
+import SearchBar from "../../../components/searchbar/searchbar";
+
 export default function Requests() {
+  const handleSearch = (query: string) => {
+    console.log("Search query:", query);
+  };
   return (
     <>
       <Header />
       <Sidebar />
       <Container>
         <Label label="Request" />
+        <div className="request-container">
+          <div className="request-row">
+            <SearchBar onSearch={handleSearch} />
+          </div>
+        </div>
       </Container>
     </>
   );

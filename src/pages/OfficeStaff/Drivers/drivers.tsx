ea18b83f5bb32e61117e4e44ef37c7d330ea087f
rakss17 <bohariambulo17@gmail.com>
2023-07-16 18:03:00 +0800

    initial drivers page

--- src/pages/OfficeStaff/Drivers/drivers.tsx
+++ src/pages/OfficeStaff/Drivers/drivers.tsx
@@ -1,14 +1,67 @@
+import { useState, useEffect } from "react";
 import Header from "../../../components/header/header";
 import Sidebar from "../../../components/sidebar/sidebar";
 import Container from "../../../components/container/container";
 import "./drivers.css";
+import Label from "../../../components/label/label";
+import SearchBar from "../../../components/searchbar/searchbar";
+import Dropdown from "../../../components/dropdown/dropdown";
+
+interface Driver {
+  id: number;
+  driver_name: string;
+  capacity: number;
+  vehicle_type: string;
+  vehicle_image: string;
+  status: string;
+}
 export default function Drivers() {
+  const [drivers, setDrivers] = useState<Driver[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const fetchedDriverList = () => {
+    // setDrivers(fetchedDrivers);
+  };
+
+  useEffect(() => {
+    fetchedDriverList();
+  }, []);
+
+  const handleSearchChange = (term: string) => {
+    setSearchTerm(term);
+  };
+
+  const filteredDriverList = drivers.filter((driver) => {
+    const isCategoryMatch =
+      selectedCategory === null || driver.status === selectedCategory;
+
+    const isSearchMatch =
+      searchTerm === "" ||
+      driver.driver_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      driver.status.toLowerCase().includes(searchTerm.toLowerCase());
+
+    return isCategoryMatch && isSearchMatch;
+  });
+
+  const handleCategoryChange = (status: string) => {
+    setSelectedCategory(status === "All" ? null : status);
+  };
+
   return (
     <>
       <Header />
       <Sidebar />
       <Container>
-        <p>HELLO</p>
+        <div className="margin-top-drivers">
+          <Label label="Drivers" />
+        </div>
+        <div className="drivers-row">
+          <SearchBar onSearchChange={handleSearchChange} />
+          <Dropdown
+            status={["All", "Available", "On Trip", "Unavailable"]}
+            onCategoryChange={handleCategoryChange}
+          />
+        </div>
       </Container>
     </>
   );

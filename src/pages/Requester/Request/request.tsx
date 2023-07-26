64373d3e43930f07f7a255c978ca20cab95cd716
rakss17 <bohariambulo17@gmail.com>
2023-07-26 22:43:33 +0800

    request for requester page

--- src/pages/Requester/Request/request.tsx
+++ src/pages/Requester/Request/request.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { faColumns, faClipboardList } from "@fortawesome/free-solid-svg-icons";
 import Header from "../../../components/header/header";
 import Sidebar from "../../../components/sidebar/sidebar";
@@ -16,41 +16,133 @@ const sidebarData: SidebarItem[] = [
   { icon: faColumns, text: "Dashboard", path: "/DashboardR" },
   { icon: faClipboardList, text: "Request", path: "/Request" },
 ];
+const fetchedPendingData = [
+  {
+    request_number: "02",
+    travel_date: "August 17, 2023",
+    vehicle: "Ford Ranger",
+  },
+  {
+    request_number: "03",
+    travel_date: "August 18, 2023",
+    vehicle: "Nissan Navara",
+  },
+  {
+    request_number: "04",
+    travel_date: "August 19, 2023",
+    vehicle: "Chevrolet Colorado",
+  },
+  {
+    request_number: "05",
+    travel_date: "August 20, 2023",
+    vehicle: "Honda Ridgeline",
+  },
+  {
+    request_number: "06",
+    travel_date: "August 21, 2023",
+    vehicle: "Mitsubishi Triton",
+  },
+  {
+    request_number: "07",
+    travel_date: "August 22, 2023",
+    vehicle: "Isuzu D-Max",
+  },
+  {
+    request_number: "08",
+    travel_date: "August 23, 2023",
+    vehicle: "Volkswagen Amarok",
+  },
+  {
+    request_number: "09",
+    travel_date: "August 24, 2023",
+    vehicle: "Mazda BT-50",
+  },
+  {
+    request_number: "10",
+    travel_date: "August 25, 2023",
+    vehicle: "GMC Canyon",
+  },
+  {
+    request_number: "11",
+    travel_date: "August 26, 2023",
+    vehicle: "RAM 1500",
+  },
+];
 
-const fetchedAprrovedData = [
+const fetchedApprovedData = [
+  {
+    request_number: "10",
+    travel_date: "April 16, 2024",
+    vehicle: "Toyota hilux",
+  },
+  {
+    request_number: "02",
+    travel_date: "August 17, 2023",
+    vehicle: "Ford Ranger",
+  },
+  {
+    request_number: "03",
+    travel_date: "August 18, 2023",
+    vehicle: "Nissan Navara",
+  },
+  {
+    request_number: "04",
+    travel_date: "August 19, 2023",
+    vehicle: "Chevrolet Colorado",
+  },
+];
+const fetchedCanceledData = [
   {
     request_number: "10",
     travel_date: "April 16, 2024",
     vehicle: "Toyota hilux",
   },
 ];
+const fetchedDeclinedData = [
+  {
+    request_number: "10",
+    travel_date: "April 16, 2024",
+    vehicle: "Toyota hilux",
+  },
+  {
+    request_number: "10",
+    travel_date: "August 25, 2023",
+    vehicle: "GMC Canyon",
+  },
+  {
+    request_number: "11",
+    travel_date: "August 26, 2023",
+    vehicle: "RAM 1500",
+  },
+];
 
 export default function Request() {
   const [requestData, setRequestData] = useState<any[]>([]);
+  const [selectedStatus, setSelectedStatus] = useState<string>("Pending");
 
   const handleButtonClick = (status: string) => {
     switch (status) {
       case "Pending":
-        setRequestData([
-          "Request 1 (Pending)",
-          "Request 2 (Pending)",
-          "Request 3 (Pending)",
-        ]);
+        setRequestData(fetchedPendingData);
         break;
       case "Approved":
-        setRequestData(fetchedAprrovedData);
+        setRequestData(fetchedApprovedData);
         break;
       case "Canceled":
-        setRequestData(["Request 6 (Canceled)"]);
+        setRequestData(fetchedCanceledData);
         break;
       case "Declined":
-        setRequestData(["Request 7 (Declined)", "Request 8 (Declined)"]);
+        setRequestData(fetchedDeclinedData);
         break;
       default:
         setRequestData([]);
         break;
     }
+    setSelectedStatus(status);
   };
+  useEffect(() => {
+    handleButtonClick("Pending");
+  }, []);
 
   return (
     <>
@@ -62,29 +154,41 @@ export default function Request() {
         </div>
 
         <div className="nav-button-row">
-          <button onClick={() => handleButtonClick("Pending")}>Pending</button>
-          <button onClick={() => handleButtonClick("Approved")}>
+          <button
+            onClick={() => handleButtonClick("Pending")}
+            className={selectedStatus === "Pending" ? "active" : ""}
+          >
+            Pending
+          </button>
+          <button
+            onClick={() => handleButtonClick("Approved")}
+            className={selectedStatus === "Approved" ? "active" : ""}
+          >
             Approved
           </button>
-          <button onClick={() => handleButtonClick("Canceled")}>
+          <button
+            onClick={() => handleButtonClick("Canceled")}
+            className={selectedStatus === "Canceled" ? "active" : ""}
+          >
             Canceled
           </button>
-          <button onClick={() => handleButtonClick("Declined")}>
+          <button
+            onClick={() => handleButtonClick("Declined")}
+            className={selectedStatus === "Declined" ? "active" : ""}
+          >
             Declined
           </button>
         </div>
         <div className="requests-container">
           <table>
-            <thead>
-              <tr>
-                <p>Request No.</p>
-                <p>Travel Date</p>
-                <p>Vehicle</p>
-              </tr>
+            <thead className="requests-table-header">
+              <p>Request No.</p>
+              <p>Travel Date</p>
+              <p>Vehicle</p>
             </thead>
             <tbody>
               {requestData.map((request, index) => (
-                <tr key={index}>
+                <tr className="requests-list" key={index}>
                   <td>{request.request_number}</td>
                   <td>{request.travel_date}</td>
                   <td>{request.vehicle}</td>

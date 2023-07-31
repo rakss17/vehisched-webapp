8632384ef2b4a25485bc3d07b1cc9cc56f682f96
rakss17 <bohariambulo17@gmail.com>
2023-07-31 19:38:54 +0800

    vehicle admin page

--- src/pages/Admin/admin.tsx
+++ src/pages/Admin/admin.tsx
@@ -4,6 +4,70 @@ import Container from "../../components/container/container";
 import Label from "../../components/label/label";
 import "./admin.css";
 import SearchBar from "../../components/searchbar/searchbar";
+import ToyotaHilux from "../../components/images/toyota-hilux.jpg";
+import MitsubishiMontero from "../../components/images/mitsubishi-montero.jpg";
+import Fortuner from "../../components/images/fortuner.jpg";
+import ToyotaHiace from "../../components/images/toyota-hiace.png";
+import Ellipsis from "../../components/ellipsismenu/ellipsismenu";
+
+interface Vehicle {
+  id: number;
+  vehicle_name: string;
+  capacity: number;
+  vehicle_type: string;
+  vehicle_image: string;
+  status: string;
+}
+const fetchedVehicles: Vehicle[] = [
+  {
+    id: 1,
+    vehicle_name: "KDA 1368 Toyota Hilux",
+    capacity: 5,
+    vehicle_type: "Pickup Truck",
+    vehicle_image: ToyotaHilux,
+    status: "On Trip",
+  },
+  {
+    id: 2,
+    vehicle_name: "KCU 2522 Montero Sport",
+    capacity: 7,
+    vehicle_type: "SUV",
+    vehicle_image: MitsubishiMontero,
+    status: "Available",
+  },
+  {
+    id: 3,
+    vehicle_name: "KAB 2855 Fortuner",
+    capacity: 7,
+    vehicle_type: "SUV",
+    vehicle_image: Fortuner,
+    status: "On Trip",
+  },
+  {
+    id: 4,
+    vehicle_name: "KYZ 2069 Toyota Hiace",
+    capacity: 15,
+    vehicle_type: "Van",
+    vehicle_image: ToyotaHiace,
+    status: "Available",
+  },
+  {
+    id: 5,
+    vehicle_name: "KDA 1368 Toyota Hilux",
+    capacity: 5,
+    vehicle_type: "Pickup Truck",
+    vehicle_image: ToyotaHilux,
+    status: "Unavailable",
+  },
+  {
+    id: 6,
+    vehicle_name: "KYZ 2069 Toyota Hiace",
+    capacity: 15,
+    vehicle_type: "Van",
+    vehicle_image: ToyotaHiace,
+    status: "Unavailable",
+  },
+];
 
 const fetchedRequesterData = [
   {
@@ -79,12 +143,21 @@ export default function Admin() {
   const [displayAccounts, setDisplayAccounts] = useState(true);
   const [displayVehicles, setDisplayVehicles] = useState(false);
   const [accountsData, setAccountsData] = useState<any[]>([]);
+  const [vehiclesData, setVehiclesData] = useState<Vehicle[]>([]);
   const [selectedNavigation, setSelectedNavigation] =
     useState<string>("Accounts");
   const [selectedAccountNavigation, setSelectedAccountNavigation] =
     useState<string>("Requester");
+  const [searchAccountTerm, setSearchAccountTerm] = useState("");
+  const [searchVehicleTerm, setSearchVehicleTerm] = useState("");
 
-  const [searchTerm, setSearchTerm] = useState("");
+  const fetchedVehicleList = () => {
+    setVehiclesData(fetchedVehicles);
+  };
+
+  useEffect(() => {
+    fetchedVehicleList();
+  }, []);
 
   const handleButtonClick = (nav: string) => {
     switch (nav) {
@@ -131,17 +204,41 @@ export default function Admin() {
   }, []);
   const filteredAccountsData = accountsData.filter((account) => {
     const isSearchMatch =
-      searchTerm === "" ||
-      account.id_number.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      account.last_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      account.first_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      account.contact_number.toLowerCase().includes(searchTerm.toLowerCase());
+      searchAccountTerm === "" ||
+      account.id_number
+        .toLowerCase()
+        .includes(searchAccountTerm.toLowerCase()) ||
+      account.last_name
+        .toLowerCase()
+        .includes(searchAccountTerm.toLowerCase()) ||
+      account.first_name
+        .toLowerCase()
+        .includes(searchAccountTerm.toLowerCase()) ||
+      account.contact_number
+        .toLowerCase()
+        .includes(searchAccountTerm.toLowerCase());
+
+    return isSearchMatch;
+  });
+  const filteredVehicleList = vehiclesData.filter((vehicle) => {
+    const isSearchMatch =
+      searchVehicleTerm === "" ||
+      vehicle.vehicle_name
+        .toLowerCase()
+        .includes(searchVehicleTerm.toLowerCase()) ||
+      vehicle.vehicle_type
+        .toLowerCase()
+        .includes(searchVehicleTerm.toLowerCase()) ||
+      vehicle.status.toLowerCase().includes(searchVehicleTerm.toLowerCase());
 
     return isSearchMatch;
   });
 
-  const handleSearchChange = (term: string) => {
-    setSearchTerm(term);
+  const handleSearchAccount = (term: string) => {
+    setSearchAccountTerm(term);
+  };
+  const handleSearchVehicle = (term: string) => {
+    setSearchVehicleTerm(term);
   };
   return (
     <>
@@ -167,7 +264,7 @@ export default function Admin() {
         {displayAccounts && (
           <div className="display-accounts-container">
             <div className="accounts-row">
-              <SearchBar onSearchChange={handleSearchChange} />
+              <SearchBar onSearchChange={handleSearchAccount} />
               <button>Add User {""}+</button>
             </div>
             <div className="usertype-button-row">
@@ -254,7 +351,43 @@ export default function Admin() {
             </div>
           </div>
         )}
-        {displayVehicles && <p>Vehicles</p>}
+        {displayVehicles && (
+          <>
+            <div className="accounts-row">
+              <SearchBar onSearchChange={handleSearchVehicle} />
+              <button>Add Vehicle {""}+</button>
+            </div>
+            <div className="vehicles-container">
+              {filteredVehicleList.length === 0 ? (
+                <p className="vehicles-null">No vehicles available</p>
+              ) : (
+                filteredVehicleList.map((vehicle) => (
+                  <a className="vehicle-card">
+                    <div className="vehicle-row">
+                      <div className="vehicle-column">
+                        <p className="vehicle-name">{vehicle.vehicle_name}</p>
+                        <p className="vehicle-detail">
+                          Seating Capacity: {vehicle.capacity}
+                        </p>
+                        <p className="vehicle-detail">
+                          Type: {vehicle.vehicle_type}
+                        </p>
+                        <p className="vehicle-status">{vehicle.status}</p>
+                      </div>
+                      <img
+                        className="vehicle-image"
+                        src={vehicle.vehicle_image}
+                      />
+                      <div className="ellipsis-container">
+                        <Ellipsis status={["Edit", "Delete"]} />
+                      </div>
+                    </div>
+                  </a>
+                ))
+              )}
+            </div>
+          </>
+        )}
       </Container>
     </>
   );

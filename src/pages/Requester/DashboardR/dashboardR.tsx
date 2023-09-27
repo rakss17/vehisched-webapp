66f4cb37e16d44e9526070b845bd864d281423fc
rakss17 <bohariambulo17@gmail.com>
2023-09-27 16:17:02 +0800

    vehicle websocket separated

--- src/pages/Requester/DashboardR/dashboardR.tsx
+++ src/pages/Requester/DashboardR/dashboardR.tsx
@@ -14,6 +14,7 @@ import { fetchVehiclesAPI } from "../../../components/api/api";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../redux/store";
 import { serverSideUrl } from "../../../components/api/api";
+import { VehicleAvailableWebsocket } from "../../../components/api/websocket";
 
 const sidebarData: SidebarItem[] = [
   { icon: faColumns, text: "Dashboard", path: "/DashboardR" },
@@ -30,46 +31,7 @@ export default function DashboardR() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const newSocket = new WebSocket(
-      "ws://localhost:8000/ws/vehicle/available/"
-    );
-
-    newSocket.onopen = (event) => {
-      console.log("WebSocket connection opened");
-      newSocket.send(
-        JSON.stringify({
-          action: "fetch_available_vehicles",
-        })
-      );
-    };
-
-    newSocket.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      console.log("Received WebSocket message:", data);
-      if (data.type === "available.vehicles") {
-        setVehiclesData(data.data);
-      } else if (data.type === "status.update") {
-        console.log("Received status update:", data.message);
-        const plateNumber = data.data.plate_number;
-        if (data.message.includes("added")) {
-          setVehiclesData((prevVehicles) => [...prevVehicles, data.data]);
-        } else if (data.message.includes("removed")) {
-          setVehiclesData((prevVehicles) =>
-            prevVehicles.filter(
-              (vehicle) => vehicle.plate_number !== plateNumber
-            )
-          );
-        }
-      }
-    };
-
-    newSocket.onclose = (event) => {
-      console.log("WebSocket connection closed");
-    };
-
-    return () => {
-      newSocket.close();
-    };
+    VehicleAvailableWebsocket(setVehiclesData);
   }, []);
 
   const handleSetTripModal = () => {
